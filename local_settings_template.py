90e254f91868e13c8ddbb2a20f08aa2fe95a3ffe
Ken Vollmer <vormooth@gmail.com>
2009-05-29 18:37:37 +0000

    file to make local_settings.py from

---
+++ local_settings_template.py
@@ -0,0 +1,9 @@
+# local settings
+
+DATABASE_USER = '' # like: 'postgres'
+DATABASE_PASSWORD = '' # like: '1234'
+
+MEDIA_ROOT = '' # like: 'U:/dev/gwst/media' - where the Django development server goes to look for your static files
+MEDIA_URL = '/site-media/' # the URL dir through which your web server serves static pages and images
+
+GMAPS_API_KEY = '' # like: a big long string of characters you get from Google
