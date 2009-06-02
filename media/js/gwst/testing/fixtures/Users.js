70b463d78c6c7d3e2b3091f2909a1196f5f26450
Ken Vollmer <vormooth@gmail.com>
2009-06-02 19:40:08 +0000

    cleaned out some unneeded parts of MM media tree

--- media/js/gwst/testing/fixtures/Users.js
+++
@@ -1,9 +0,0 @@
-Ext.namespace('gwst', 'gwst.testing', 'gwst.data', 'gwst.testing.fixtures');
-
-gwst.testing.fixtures.Users = {
-    cburt: new gwst.data.User({
-        name: 'Chad Burt',
-        email: 'chad@underbluewaters.net',
-        studyRegion: gwst.testing.fixtures.StudyRegions.southCoast
-    })
-}
--- media/js/gwst/testing/urls.js
+++
@@ -1,8 +0,0 @@
-Ext.namespace('gwst', 'gwst.testing');
-
-gwst.testing.urls = {
-    newMPAForm: './js/gwst/testing/serverside/new_mpa_form.json',
-    newMPAService: './js/gwst/testing/serverside/new_mpa_service.json',
-    mpas: '/gwst/tests/mpas_public',
-    arrays : '/gwst/domains/arrays'
-}
