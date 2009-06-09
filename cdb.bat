4181c2debd90e0c846e702ab9a4378c42c4363e7
Ken Vollmer <vormooth@gmail.com>
2009-06-09 18:16:52 +0000

    added some missing db initializations

--- cdb.bat
+++ cdb.bat
@@ -1,2 +1,6 @@
 createdb -U postgres -T template_postgis gwst
 python manage.py syncdb --noinput
+python add_merc.py
+psql -d gwst -f database\functions\gwst_clip_shape_to_study_area.sql -U postgres
+psql -d gwst -f database\functions\gwst_largest_poly_in_multipoly.sql -U postgres
+psql -d gwst -f database\functions\gwst_validate_shape.sql -U postgres
