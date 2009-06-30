804a729273a809f683ba55f80f79b7b9a342550a
Tim Welch <tim.j.welch@gmail.com>
2009-06-30 21:26:07 +0000

    update local settings template to include email settings and more database settings

--- local_settings_template.py
+++ local_settings_template.py
@@ -1,9 +1,21 @@
-# local settings
+# local settings.  Override default settings.py
 
-DATABASE_USER = '' # like: 'postgres'
+DATABASE_NAME = ''
+DATABASE_USER = ''
 DATABASE_PASSWORD = '' # like: '1234'
-
 MEDIA_ROOT = '' # like: 'U:/dev/gwst/media' - where the Django development server goes to look for your static files
-MEDIA_URL = '/site-media/' # the URL dir through which your web server serves static pages and images
+MEDIA_URL = '' # the URL dir through which your web server serves static pages and images
 
 GMAPS_API_KEY = '' # like: a big long string of characters you get from Google
+
+TEMP_DIR = '' # Like /tmp
+
+ACCOUNT_ACTIVATION_DAYS = 7
+
+DEFAULT_FROM_EMAIL=''
+BCC_EMAIL='' # Addresses to blind carbon copy on each automated registration (SurveyMonkey)
+EMAIL_HOST=''
+EMAIL_PORT=610
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
+EMAIL_USE_TLS = True
