42600617d89239e24a3b36427e71380561af8b46
Tim Welch <tim.j.welch@gmail.com>
2009-07-30 20:30:09 +0000

    add sites to fixtures

--- dump_fixtures.sh
+++ dump_fixtures.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py dumpdata --indent=2 auth.user gwst_app > gwst_app/fixtures/initial_data.json
+python manage.py dumpdata --indent=2 auth.user gwst_app sites > gwst_app/fixtures/initial_data.json
