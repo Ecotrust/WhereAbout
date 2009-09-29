843b4e686fc0318dab97634b805a59e1a09ac7c1
Tim Welch <tim.j.welch@gmail.com>
2009-09-29 20:05:53 +0000

    update script to dump user surveys

--- dump_user_surveys.sh
+++ dump_user_surveys.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-python manage.py dumpdata --indent=2 auth.User gwst_app.InterviewGroupMembership gwst_app.GroupMemberResource gwst_app.InterviewAnswer gwst_app.InterviewStatus gwst_app.InterviewShape > gwst_app/fixtures/user_surveys.json
+
+EXPECTED_ARGS=1
+E_BADARGS=65
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  echo "Expected argument with file to save fixture as"
+  exit $E_BADARGS
+fi
+
+python manage.py dumpdata --indent=2 auth.User gwst_app.InterviewGroupMembership gwst_app.GroupMemberResource gwst_app.InterviewAnswer gwst_app.InterviewStatus gwst_app.InterviewShape > fixtures/$1
