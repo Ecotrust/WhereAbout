3bfaaca5b80ef003f1efa49175538b31860855c3
Tim Welch <tim.j.welch@gmail.com>
2009-12-22 16:19:00 +0000

    add draw tool window

---
+++ media2/js/widgets/DrawToolWindow.js
@@ -0,0 +1,42 @@
+Ext.namespace('gwst', 'gwst.widgets');
+
+gwst.widgets.DrawToolWindow = Ext.extend(Ext.Window, {
+    
+    // Constructor Defaults, can be overridden by user's config object
+    initComponent: function(){		
+		this.addEvents('cancel-res-shape');
+		this.addEvents('redo-res-shape');
+		
+		Ext.apply(this, {          
+            height: 25,
+            layout:'fit',
+            html:'blort',
+            closable: false,
+            tbar: [{
+				text: 'Cancel Shape',
+				iconCls: 'cancel-draw',
+				handler: this.cancelShape,
+				scope: this
+            },{
+            	xtype:'tbseparator'
+            },{
+				text: 'Redo Shape',
+				iconCls: 'redo-draw',
+				handler: this.redoShape,
+				scope: this
+			}]                   
+        });
+		gwst.widgets.DrawToolWindow.superclass.initComponent.call(this);		
+	},
+	
+	cancelShape: function() {
+		this.fireEvent('cancel-res-shape');
+	},
+	
+	redoShape: function() {
+		this.fireEvent('redo-res-shape');
+	}
+});
+
+//register xtype to allow for lazy initialization
+Ext.reg('gwst-draw-tool-window', gwst.widgets.DrawToolWindow);
