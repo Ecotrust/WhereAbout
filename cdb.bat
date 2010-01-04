c839d93ecccbb796dfe3123fee79d3c816172cad
Tim Welch <tim.j.welch@gmail.com>
2010-01-04 16:13:08 +0000

    create new fixture with clip region fully tied to interviews.  cdb scripts updated removing all stored procedures, yay.  the clip region table is now restored before the syncdb as tables will be created by django that will need to tie immediately to that data.  in the future we could simply bring the clip region tables into the fixtures but the shorelines multipolygons are just plain massive.  so we load them manually first

--- cdb.bat
+++ cdb.bat
@@ -1,8 +1,4 @@
 createdb -U postgres -T template_postgis gwst-or-rec
+psql -d gwst-or-rec -f database\data\CaliOregonBoundaries.sql -U postgres
 python manage.py syncdb --noinput
 python add_merc.py
-psql -d gwst-or-rec -f database\functions\gwst_clip_shape_to_study_area.sql -U postgres
-psql -d gwst-or-rec -f database\functions\gwst_clip_shape_to_shoreline.sql -U postgres
-psql -d gwst-or-rec -f database\functions\gwst_largest_poly_in_multipoly.sql -U postgres
-psql -d gwst-or-rec -f database\functions\gwst_validate_shape.sql -U postgres
-psql -d gwst-or-rec -f database\data\CaliOregonBoundaries.sql -U postgres
--- cdb.sh
+++ cdb.sh
@@ -4,9 +4,5 @@ DB="spatial-survey-demo"
 export PYTHONPATH=$PYTHONPATH:/usr/local/django-trunk/:/usr/local/django-apps/:.
 
 createdb -T gis_template $DB
-python manage.py syncdb --noinput
-psql -d $DB -f database/functions/gwst_clip_shape_to_study_area.sql
-psql -d $DB -f database/functions/gwst_clip_shape_to_shoreline.sql
-psql -d $DB -f database/functions/gwst_largest_poly_in_multipoly.sql
-psql -d $DB -f database/functions/gwst_validate_shape.sql
 psql -d $DB -f database/data/CaliOregonBoundaries.sql
+python manage.py syncdb --noinput
