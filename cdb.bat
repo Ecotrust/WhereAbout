b5c4a0f7500c968ca3f1da57046fc6edd7b90bdd
Tim Welch <tim.j.welch@gmail.com>
2010-01-08 18:27:06 +0000

    load merc proj first

--- cdb.bat
+++ cdb.bat
@@ -1,4 +1,4 @@
 createdb -U postgres -T template_postgis gwst-or-rec
+python add_merc.py
 psql -d gwst-or-rec -f database\data\CaliOregonBoundaries.sql -U postgres
 python manage.py syncdb --noinput
-python add_merc.py
