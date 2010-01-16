0b618d49c87cd9ca75a48f2f0437d54ca5b371f3
Tim Welch <tim.j.welch@gmail.com>
2010-01-16 19:14:55 +0000

    update cdb script to load new clip region

--- cdb.sh
+++ cdb.sh
@@ -4,5 +4,5 @@ DB="spatial-survey-demo"
 export PYTHONPATH=$PYTHONPATH:/usr/local/django-trunk/:/usr/local/django-apps/:.
 
 createdb -T gis_template $DB
-psql -d $DB -f database/data/CaliOregonBoundaries.sql
+psql -d $DB -f database/data/OregonRecClipRegion.sql
 python manage.py syncdb --noinput
