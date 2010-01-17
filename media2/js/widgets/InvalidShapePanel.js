d3b68f062b7f61e17e66dc18d770d100764e3f1f
Tim Welch <tim.j.welch@gmail.com>
2010-01-17 07:22:11 +0000

    add images to invalid panels

--- media2/js/widgets/InvalidShapePanel.js
+++ media2/js/widgets/InvalidShapePanel.js
@@ -25,14 +25,15 @@ gwst.widgets.InvalidShapePanel = Ext.extend(gwst.widgets.WestPanel, {
     },
     
     getText: function() {
-        var html_text = '<p class="error_text"><img src="/site-media/images/exclamation.png"> <b>There was a problem</b></p><br/>';
+        var html_text = '<p class="error_text"><b>There was a problem</b> <img src="/site-media/images/exclamation.png"></p><br/>';
         if (this.status_code == 2) {
-            html_text += '<p>Your '+this.shape_name+' is not valid because it overlaps itself.  It needs to be a single closed loop (see example below)</p>';
+            html_text += '<p>Your '+this.shape_name+' is not valid because it overlaps itself (example below).</p> <img class="invalid-image" src="/site-media/images/invalid_bowtie.png"><br/>';
         } else if (this.status_code == 3) {
-            html_text += '<p>Your '+this.shape_name+' falls completely outside the area of interest which is the Pacific Ocean off the Oregon Coast.  Rivers, estuaries and lakes are excluded from this survey.  <br/><br/>In the example below the '+this.shape_name+' falls within Coos Bay which is not valid.  Outside the mouth of Coos Bay would be.';
+            html_text += '<p>Your '+this.shape_name+' falls completely outside the area of interest which is the Pacific Ocean off the Oregon Coast.  Rivers, estuaries and lakes are excluded from this survey.  <br/><br/>In the example below the '+this.shape_name+' falls within Coos Bay which is not valid.  Outside the mouth of Coos Bay would be.  <img class="invalid-image" src="/site-media/images/invalid_bounds.png"><br/>';
         } else if (this.status_code == 4) {
-            html_text += '<p>Your new '+this.shape_name+' overlaps one of your other '+this.resource+' '+this.shape_name+'s.  They are not allowed to do this.  If you have two that border each other, just draw the second one along the edge of the first as best as you can and tell us in your boundary notes that it should border the other.  We will take care of the rest.';
+            html_text += '<p>Your new '+this.shape_name+' overlaps one of your other '+this.resource+' '+this.shape_name+'s.  They are not allowed to do this.  If you have two that border each other, just draw the second one along the edge of the first as best as you can and tell us in your boundary notes that it should border the other.  We will take care of the rest.  <img class="invalid-image" src="/site-media/images/invalid_overlap.png"><br/>';
         }
+        html_text += '<br/><p>Click the \'Continue\' button to try again.</p>';
         return html_text;
     },
 	
