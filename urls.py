a75b599a5a0708f79c90e54b3150e2479d123074
Ryan Hodges <rhodges@ecotrust.org>
2011-06-03 20:59:34 +0000

    corrected name on admin-lite-login.html and adjusted admin path

--- urls.py
+++ urls.py
@@ -21,6 +21,7 @@ if settings.DEBUG is True:
     urlpatterns += patterns('',
         (r'^site-media/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
         (r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-        (r'^install-media/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.STATICFILES_ROOT, 'show_indexes': True})
+        (r'^install-media/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.STATICFILES_ROOT, 'show_indexes': True}),
+        (r'^admin/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.ADMIN_MEDIA_PREFIX, 'show_indexes': True})
     )
     
