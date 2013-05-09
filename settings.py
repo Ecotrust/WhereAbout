5ca4058596588c786d1972167e0261beed97e9c5
Ryan Hodges <rhodges@ecotrust.org>
2013-05-09 15:05:21 -0700

    updating template loader

--- settings.py
+++ settings.py
@@ -64,7 +64,7 @@ SECRET_KEY = 'set-in-local_settings'
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.app_directories.loader',
 #     'django.template.loaders.eggs.load_template_source',
 )
 
